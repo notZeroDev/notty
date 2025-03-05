693d612f290057d327e9a42fd2af0f803a148f63
notZeroDev <mohmed.ayman11@gmail.com>
2025-03-05 07:42:28 +0200

    added views and renamed css files

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 const createCard = document.querySelector(".master");
 createCard.addEventListener("click", (e) => {
   console.log("create new card");
+  window.location.href = window.location.origin + "/note";
 });
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require("path");
 app.engine("pug", require("pug").__express);
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/note", (req, res, next) => {
+  res.render("note.pug", { title: "note" });
+});
 app.use((req, res, next) => {
   console.log("we are starting");
-  res.render("layouts/main.pug");
+  res.render("main.pug", { title: "Notty" });
 });
 app.listen(3000);
