9a8e3889464b4f9180819074ea0f3bd568e46f61
notZeroDev <mohmed.ayman11@gmail.com>
2025-03-05 06:39:38 +0200

    configure mvc files

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.engine("pug", require("pug").__express);
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   console.log("we are starting");
+  res.render("layouts/main.pug");
 });
 app.listen(3000);
